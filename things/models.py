813858ad5efcaaea80be372678db047e192ca966
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-10-25 12:31:17 +0100

    add name description and quantity to things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,11 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
+
 # Create your models here.
+
+class things(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+    quantity = models.IntegerField()
+
