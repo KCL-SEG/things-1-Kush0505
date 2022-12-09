d0c70460b3b2e7418aa6b6a8819b6f2a88f32dc8
Kushaagra Kapoor <kushaagrakapoor@gmail.com>
2022-12-09 15:53:56 +0000

    Complete things model and tests

--- things/models.py
+++ things/models.py
@@ -7,7 +7,22 @@ from django.core.validators import MaxValueValidator
 # Create your models here.
 
 class things(models.Model):
-    name = models.CharField( max_length = 30, blank = False, unique = True)
-    description = models.CharField(max_length = 30, blank = False, unique = True)
-    quantity = models.IntegerField(unique = False, default = 0, validators = [MinValueValidator(0, message = "Value must be greater than or equal to 0"), MaxValueValidator(100, message = "Value must be less than or equal to 100")])
+    name = models.CharField( 
+        max_length = 30, 
+        blank = False, 
+        unique = True
+    )
+    description = models.CharField(
+        max_length = 30, 
+        blank = False, 
+        unique = True
+    )
+    quantity = models.IntegerField(
+        unique = False, 
+        default = 0, 
+        validators = [
+            MinValueValidator(0, message = "Value must be greater than or equal to 0"), 
+            MaxValueValidator(100, message = "Value must be less than or equal to 100")
+        ]
+    )
 
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,18 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from things.models import Thing
 
 # Create your tests here.
+
+class BaseModelTest(TestCase):
+    def setUp(self):
+        self.thing = Thing(name="Foobar", description = "Foobar Thing", quantity = 2)
+
+    def test_valid_thing(self):
+        self._assert_thing_is_vaild("Could not create thing with the required attributes")
+    
+    def _assert_thing_is_valid(self, message="a valid thing was rejected"):
+        try:
+            self.thing.full_clean()
+        except ValidationError:
+            self.fail(message)
